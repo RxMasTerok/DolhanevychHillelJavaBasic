9a658eb0bf27683c0815f3091f8c28d32ac22b87
AndriiDolhanevych <Dolhanevych@gmail.com>
2024-02-12 20:22:29 +0200

    init commit

---
+++ src/HomeworkTwo/HomeworkTwo.java
@@ -0,0 +1,13 @@
+package HomeworkTwo;
+
+public class HomeworkTwo {
+    public static void main(String[] args) {
+
+        double northernLatitude = 48.8582573;
+        double easternLongitude = 2.2945111;
+
+        System.out.print("Coordinates Eiffel Tower =  ");
+        System.out.print("Northern Latitude " + northernLatitude + "°");
+        System.out.print(" Eastern Longitude " + easternLongitude + "°");
+    }
+}
